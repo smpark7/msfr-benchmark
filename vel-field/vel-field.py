e0b5a55ad85c9fd12449861b15c59faa8541bd84
Sun Myung Park <park_cg@live.com>
2020-08-12 17:13:53 +0800

    Edit tables, add vel-field.py csv parse script

---
+++ vel-field/vel-field.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+aa = pd.read_csv('vel-field-stabilized_csv_aa_0064.csv')
+bb = pd.read_csv('vel-field-stabilized_csv_bb_0064.csv')
+
+fig, ax = plt.subplots()
+ax.plot(bb['ux'] / 100, bb['y'] / 100)
+ax.set_ylim(0, 2)
+ax.set_xlim(-.2, .6)
+
+x = np.arange(0, 201, 25)
+aa_ux = np.zeros(len(x))
+aa_uy = np.zeros(len(x))
+bb_ux = np.zeros(len(x))
+bb_uy = np.zeros(len(x))
+
+for i in range(len(x)):
+    aa_ux[i] = aa['ux'][x[i]]
+    aa_uy[i] = aa['uy'][x[i]]
+    bb_ux[i] = bb['ux'][x[i]]
+    bb_uy[i] = bb['uy'][x[i]]
+
+print(aa_ux)
+print(aa_uy)
+print(bb_ux)
+print(bb_uy)
